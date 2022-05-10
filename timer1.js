bd9cf391b1eb575efee6569aa27c81db538db394
Ik Sung Choi <ikchoi0@gmail.com>
2022-05-10 19:31:30 -0400

    fixed logic in if

--- timer1.js
+++ timer1.js
@@ -1,18 +1,11 @@
 const alramArr = process.argv.slice(2);
 
 for (let i = 0; i < alramArr.length; i++) {
-  if (!alramArr[i]) {
-    process.stdout.write('beep');
-    continue;
-  }
-  if (alramArr[i] < 0) {
-    continue;
-  }
-  if (typeof Number(alramArr[i]) !== 'number') {
+  if (!alramArr[i] || Number(alramArr[i]) < 0 || Number(alramArr[i]) === NaN) {
     continue;
   }
   setTimeout(() => {
-    process.stdout.write('\x07');
+    process.stdout.write("\x07");
     process.stdout.write(`alram at ${alramArr[i]} seconds\n`);
   }, 1000 * Number(alramArr[i]));
 }
